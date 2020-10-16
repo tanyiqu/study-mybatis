d4c6e4e8a3e920defb4abaa7fa5cc552276e5c42
Tanyiqu <1953649096@qq.com>
2020-10-16 17:19:59 +0800

    单表CRUD操作

--- mybatis-study/01-mybatis/src/main/java/com/tanyiqu/mapper/IUserMapper.java
+++ mybatis-study/01-mybatis/src/main/java/com/tanyiqu/mapper/IUserMapper.java
@@ -5,5 +5,18 @@ import com.tanyiqu.pojo.User;
 import java.util.List;
 
 public interface IUserMapper {
+    // 查询所有用户
     List<User> getUsers();
+
+    // 根据id查用户
+    User getUserById(int id);
+
+    // 添加一个用户
+    int addUser(User user);
+
+    // 修改用户
+    int updateUser(User user);
+
+    // 删除用户
+    int deleteUser(int id);
 }
--- mybatis-study/01-mybatis/src/test/java/com/tanyiqu/mapper/UserMapperTest.java
+++ mybatis-study/01-mybatis/src/test/java/com/tanyiqu/mapper/UserMapperTest.java
@@ -11,15 +11,59 @@ public class UserMapperTest {
     @Test
     public void getUsers() {
         SqlSession sqlSession = MybatisUtil.getSqlSession();
-
         IUserMapper userMapper = sqlSession.getMapper(IUserMapper.class);
 
         List<User> users = userMapper.getUsers();
-
         for (User user : users) {
             System.out.println(user);
         }
 
         sqlSession.close();
     }
+
+    @Test
+    public void getUserById() {
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        IUserMapper userMapper = sqlSession.getMapper(IUserMapper.class);
+
+        User user = userMapper.getUserById(4);
+        System.out.println(user);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void addUser() {
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        IUserMapper userMapper = sqlSession.getMapper(IUserMapper.class);
+
+        userMapper.addUser(new User(4,"张四","123654"));
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+
+    @Test
+    public void updateUser(){
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        IUserMapper userMapper = sqlSession.getMapper(IUserMapper.class);
+
+        userMapper.updateUser(new User(4,"张四","000000"));
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser(){
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
+        IUserMapper userMapper = sqlSession.getMapper(IUserMapper.class);
+
+        userMapper.deleteUser(4);
+
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
 }
