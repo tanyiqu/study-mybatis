e557d8b5a2de23575c2ce65f24f55883b232f8a3
Tanyiqu <1953649096@qq.com>
2020-10-21 13:39:19 +0800

    复杂查询环境搭建

---
+++ mybatis-study/06-mybatis/src/main/java/com/tanyiqu/mapper/TeacherMapper.java
@@ -0,0 +1,12 @@
+package com.tanyiqu.mapper;
+
+import com.tanyiqu.pojo.Teacher;
+
+import java.util.List;
+
+public interface TeacherMapper {
+
+
+    List<Teacher> getTeachers();
+
+}
---
+++ mybatis-study/06-mybatis/src/main/java/com/tanyiqu/pojo/Teacher.java
@@ -0,0 +1,36 @@
+package com.tanyiqu.pojo;
+
+public class Teacher {
+
+    int id;
+    String name;
+
+    public Teacher(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
